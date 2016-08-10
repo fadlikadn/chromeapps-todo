ec3b30d329d60cc77bb981534346c16b42adc603
Fadlika Dita Nurjanto <fadlikadn@gmail.com>
2016-08-10 20:49:59 +0700

    Create alarm in chrome apps

--- js/alarms.js
+++ js/alarms.js
@@ -1,3 +1,12 @@
+// Reference : https://developer.chrome.com/apps/app_codelab_alarms#launch
 (function () {
 	'use strict';
+	var alarmName = 'remindme';
+	function checkAlarm (callback) {
+		chrome.alarms.getAll(function (alarms) {
+			var hasAlarm = alarms.some(function (a) {
+				return a.name == alarmName;
+			})
+		})
+	}
 })
